5380a08c5bcc88943070f4ffc0dd74ca129374d0
Luke Cahill <lmc@outlook.com>
2016-12-07 18:17:45 +0000

    Add unit tests

---
+++ SGBUnitTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Security_Group_Builder;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SGBUnitTests {
+	[TestClass]
+	public class UnitTest1 {
+		Builder builder = new Builder();
+
+		[TestMethod]
+		public void splitPorts() {
+			var sg = new SecurityGroup {
+				Port = "997"
+			};
+
+			var result = builder.splitPorts(sg);
+			var expected = new string[] { "997", "997" };
+			Assert.IsTrue(result.SequenceEqual(expected));
+		}
+
+		public void writeToFile() { }
+	}
+}
