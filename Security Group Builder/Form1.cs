a38d80183da9beffe3b5047c98bca23a2cf1c0e5
Luke Cahill <lmc@outlook.com>
2017-01-11 19:59:58 +0000

    Include link to source in help message

--- Security Group Builder/Form1.cs
+++ Security Group Builder/Form1.cs
@@ -15,7 +15,7 @@ namespace Security_Group_Builder {
 		}
 
 		private void helpToolStripMenuItem_Click(object sender, EventArgs e) {
-			MessageBox.Show("The format for this should be NAME DESCRIPTION DIRECTION PROTOCOL PORT IPADDRESS.\n\nThese should be tab separated values.\n\nThe IPADDRESS can be either a CidrIp or a Security group name.\n\nFormatting can be done using the formatting tool of your choice.\n\nCreated by Luke Cahill", "Help", MessageBoxButtons.OK);
+			MessageBox.Show("The format for this should be NAME DESCRIPTION DIRECTION PROTOCOL PORT IPADDRESS.\n\nThese should be tab separated values.\n\nThe IPADDRESS can be either a CidrIp or a Security group name.\n\nFormatting can be done using the formatting tool of your choice.\n\nCreated by Luke Cahill\n\nSource: https://github.com/lukecahill/AWS-Security-Group-Builder", "Help", MessageBoxButtons.OK);
 		}
 	}
 }
